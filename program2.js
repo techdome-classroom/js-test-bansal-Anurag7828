14b47b18ea363efcb8d1beb8f86bb5f5d1a52b62
Anurag7828 <141642380+Anurag7828@users.noreply.github.com>
2024-02-23 12:58:21 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,13 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+   
+    const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+    const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const hrns = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const ths = ["", "M", "MM", "MMM"];
+    return ths[Math.floor(s / 1000)] + hrns[Math.floor((s % 1000) / 100)] + tens[Math.floor((s % 100) / 10)] + ones[s % 10];
+};
 };
 
 
